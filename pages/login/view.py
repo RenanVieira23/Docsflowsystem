b9376a91fe71f4b0611990e9a3f4de1cee90316e
RenanVieira23 <renanvfsantos@gmail.com>
2026-04-20 19:39:08 -0300

    alteraçoes img2

--- pages/login/view.py
+++ pages/login/view.py
@@ -157,7 +157,7 @@ def login_view(page: ft.Page, navegar):
                         height=90,
                         fit="contain",
                     ),
-                    alignment=ft.alignment.center,
+                    alignment="center",
                 ),
 
                 # fallback textual (caso imagem falhe)
