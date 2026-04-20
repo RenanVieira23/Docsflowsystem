a5d3240e00e5d201d603807c99aac6db015d8b76
RenanVieira23 <renanvfsantos@gmail.com>
2026-04-20 18:40:48 -0300

    alteraçoes imagem login2

--- pages/login/view.py
+++ pages/login/view.py
@@ -149,7 +149,7 @@ def login_view(page: ft.Page, navegar):
             [
                 ft.Container(
                     content=ft.Image(
-                        src="images/LogoDocsFlow2.png",
+                        src="images/LogoDocsFlow.jpg",
                         width=220,
                         fit="contain",
                     ),
