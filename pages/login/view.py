bfe97f905feeae400d4d6324a18f0e81fce2e2a6
RenanVieira23 <renanvfsantos@gmail.com>
2026-04-21 14:46:59 -0300

    teste foto internet

--- pages/login/view.py
+++ pages/login/view.py
@@ -148,7 +148,7 @@ def login_view(page: ft.Page, navegar):
         content=ft.Column(
             [
                 ft.Image(
-                    src="images/DocsFlowLogo.png",
+                    src="https://picsum.photos/200",
                     width=160,
                     height=44,
                     error_content=ft.Text(
